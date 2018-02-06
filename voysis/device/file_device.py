64d5be6d63dfe41a9f2f74a5c9cc6139e0e99250
Oran Kelly <okelly@voysis.com>
2018-02-06 12:12:24 +0000

    Implement delivery throttling in the file device.

--- voysis/device/file_device.py
+++ voysis/device/file_device.py
@@ -1,23 +1,44 @@
+import datetime
+import sys
+import time
+import voysis.config as config
 from voysis.device.device import Device
+
+if sys.version[0] == '2':
+    import Queue as queue
+else:
+    import queue
+
 
 class FileDevice(Device):
     def __init__(self, wav_file=None):
         Device.__init__(self)
+        self.time_between_chunks = config.get_float(config.GENERAL, 'time_between_chunks', 0.08)
+        self._queue = queue.Queue()
+        self._last_chunk_time = datetime.datetime.utcfromtimestamp(0)
         self.wav_file = wav_file
 
     def start_recording(self):
-        self.frames = []
+        self._queue.queue.clear()
         self.wav_to_frames()
 
     def stop_recording(self):
-        self.frames = []
+        self._queue.queue.clear()
 
     def generate_frames(self):
-        return iter(self.frames)
+        while not self._queue.empty():
+            data = self._queue.get_nowait()
+            if data:
+                now = datetime.datetime.now()
+                seconds_since_last = (now - self._last_chunk_time).total_seconds()
+                if seconds_since_last < self.time_between_chunks:
+                    time.sleep(self.time_between_chunks - seconds_since_last)
+                self._last_chunk_time = now
+                yield data
 
     def wav_to_frames(self):
         while True:
             data = self.wav_file.read(self.chunk_size)
             if not data:
                 break
-            self.frames.append(data)
+            self._queue.put(data)
